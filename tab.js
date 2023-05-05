1f441d504f573edca6b7dd6fbdeceb4228d4b331
yoshi23s <classa1990@gmail.com>
2023-05-05 21:18:10 +0900

    リファクタリング

--- tab.js
+++ tab.js
@@ -3,6 +3,8 @@
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
+  const navLen = $nav.length;
 
   // 初期化
   const init = () => {
@@ -20,21 +22,20 @@
 
     // 対象外のnav, contentを全て一旦リセットする
     let index = 0;
-    while(index < $nav.length){
+    while(index < navLen){
       $content[index].style.display = 'none';
-      $nav[index].classList.remove('is-active');
+      $nav[index].classList.remove(ACTIVE_CLASS);
       index++;
     }
-
+    
    // 対象のコンテンツをアクティブ化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
-    $nav[targetVal].classList.add('is-active');
-
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
   };
-  
+
   // 全nav要素に対して関数を適用・発火
   let index = 0;
-  while(index < $nav.length){
+  while(index < navLen){
     $nav[index].addEventListener('click', (e) => handleClick(e));
     index++;
   }
