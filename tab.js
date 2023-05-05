f5ee1dea1ac6ed2ce85e04c590633356fff5d86b
yoshi23s <classa1990@gmail.com>
2023-05-05 21:11:44 +0900

    タブの動きの完成

--- tab.js
+++ tab.js
@@ -1,5 +1,4 @@
 (() =>{
-  // ここに命令を書く
   const $doc = document;
   const $tab = $doc.getElementById('js-tab');
   const $nav = $tab.querySelectorAll('[data-nav]');
@@ -19,13 +18,20 @@
     const $this = e.target;
     const targetVal = $this.dataset.nav;
 
-    console.log('targetVal', targetVal);
+    // 対象外のnav, contentを全て一旦リセットする
+    let index = 0;
+    while(index < $nav.length){
+      $content[index].style.display = 'none';
+      $nav[index].classList.remove('is-active');
+      index++;
+    }
 
-  //   // 対象のコンテンツをアクティブ化する
+   // 対象のコンテンツをアクティブ化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
+    $nav[targetVal].classList.add('is-active');
+
   };
   
-
   // 全nav要素に対して関数を適用・発火
   let index = 0;
   while(index < $nav.length){
